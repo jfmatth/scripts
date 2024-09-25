e8b54bf992806f3992a59f0d51328e1dd259e2e1
John Matthew <jmatthew@publicstorage.com>
2024-09-25 16:33:10 -0700

    improvements

--- buildvm.bat
+++ buildvm.bat
@@ -6,6 +6,8 @@
 :: %2 - Type of VM
 :: %3 - image name to use (ubuntu version)
 
+set CI=C:\users\jmatthew\dev\scripts\multipass-cloudinit.yaml
+
 :: fail if parameters are blank
 IF "%1"=="" GOTO help
 IF "%2"=="" GOTO help
@@ -15,19 +17,19 @@ GOTO %2
 
 :nano
     ECHO Building nano sized VM (1cpu x 1g x 25g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=1 --memory=1g --disk=25gb %3 --cloud-init mutipass-cloudinit.yaml
+    multipass launch --name %1 --cpus=1 --memory=1g --disk=25gb %3 --cloud-init %CI%
     GOTO cleanup
 :std
     ECHO Building standard sized VM (1cpu x 2g x 25g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=1 --memory=2g --disk=25gb %3 --cloud-init .\mutipass-cloudinit.yaml
+    multipass launch --name %1 --cpus=1 --memory=2g --disk=25gb %3 --cloud-init %CI%
     GOTO cleanup
 :big
     ECHO Building big sized VM (2cpu x 4g x 50g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=2 --memory=4g --disk=50gb %3 --cloud-init .\mutipass-cloudinit.yaml
+    multipass launch --name %1 --cpus=2 --memory=4g --disk=50gb %3 --cloud-init %CI%
     GOTO cleanup
 :huge
     ECHO Building HUGE sized VM (4cpu x 8g x 50g) %1 / Importing keys / updating files, Stand by...
-    multipass launch --name %1 --cpus=4 --memory=8g --disk=50gb %3 --cloud-init .\mutipass-cloudinit.yaml
+    multipass launch --name %1 --cpus=4 --memory=8g --disk=50gb %3 --cloud-init %CI%
     GOTO cleanup
 
 
